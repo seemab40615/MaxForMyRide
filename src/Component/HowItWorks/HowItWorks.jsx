316ca4c4fa06662ebb9653dd51485d982a9bc08a
Seemab Ahmed <seemab40615@gmail.com>
2023-10-14 15:52:16 -0700

    fix

---
+++ src/Component/HowItWorks/HowItWorks.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import HowOne from '../../assets/assets/images/how-1.svg';
+import HowTwo from '../../assets/assets/images/how-2.svg';
+import HowThree from '../../assets/assets/images/how-3.svg';
+export const HowItWorks = () => {
+  return (
+    <div className="max-w-maxSize mx-auto w-full grid grid-cols-1 lg:grid-cols-3 p-5 lg:p-2">
+      <div className="grid col-span-1">
+        <div className="flex flex-col max-w-[473px] w-full gap-y-3">
+          <div>
+            <h1 className="text-[3rem] text-black font-bold">See</h1>
+            <h1 className="text-[4rem] text-black -mt-[30px] font-bold ">
+             How It Works
+            </h1>
+            <p className="text-black text-[26px] leading-[34px]">
+            Trade in or sell your vehicle to us in just a few easy steps
+            </p>
+            <button className=" bg-primary text-white border-2 rounded-[10px] max-w-[228px] px-9 min-h-[46px] mt-5">
+            Start Selling Today!
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-2">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+          <div className="col-span-1 px-4 py-6 rounded-[10px] bg-white shadow-xl">
+            <h2 className=" text-primary text-[28px] font-bold ">
+              GET
+              <br />
+              YOUR OFFER
+            </h2>
+            <p className=" text-black text-[20px] my-2">
+              Share a few details and we’ll provide a express cash offer in
+              about 2 minutes.
+            </p>
+            <img
+              src={HowOne}
+              class="img-fluid"
+              alt=""
+            />
+          </div>
+          <div className="col-span-1 px-4 py-6 rounded-[10px] bg-white shadow-xl">
+            <h2 className=" text-primary text-[28px] font-bold ">
+            TRADE
+                                                <br />
+                                                OR SELL
+            </h2>
+            <p className=" text-black text-[20px] my-2">
+            Trade in your old vehicle for a new one, or sell it to us, either way we want to buy.
+            </p>
+            <img
+              src={HowTwo}
+              class="img-fluid"
+              alt=""
+            />
+          </div>
+          <div className="col-span-1 px-4 py-6 rounded-[10px] bg-white shadow-xl">
+            <h2 className=" text-primary text-[28px] font-bold ">
+            GET
+                                                <br />
+                                                Check
+            </h2>
+            <p className=" text-black text-[20px] my-2">
+            Share a few details and we’ll provide a express cash offer in
+              about 2 minutes.
+            </p>
+            <img
+              src={HowThree}
+              class="img-fluid"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/Component/LandingPage/Landing.jsx
+++ src/Component/LandingPage/Landing.jsx
@@ -1,10 +1,11 @@
 import { HeroSection } from "../HeroSection/HeroSection";
 import { Footer } from "../footer/Footer";
-
+import { HowItWorks } from "../HowItWorks/HowItWorks";
 export const Landing = () => {
   return (
     <>
       <HeroSection />
+      <HowItWorks />
       <Footer />
     </>
   );
